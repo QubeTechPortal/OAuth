48daa7074511767dca41a87a38a2688960ead3df
AjithPanneerselvam <ajithpanneerselvam@live.com>
2017-06-05 00:30:25 +0530

    update auth

--- models/auth.js
+++
@@ -1,9 +0,0 @@
-var passport = require('passport');
-var BasicStrategy = require('passport-http').BasicStrategy;
-var User = require(./models.user);
-
-passport.use(new BasicStrategy(
-  function(username, password, callback){
-    
-  }
-))
