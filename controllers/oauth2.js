7d7a002ea38efed12fec1630e320bb566e0a88e6
AjithPanneerselvam <ajithpanneerselvam@live.com>
2017-06-07 17:17:25 +0530

    Access Grant - Access Token exchange

--- controllers/oauth2.js
+++ controllers/oauth2.js
@@ -21,3 +21,57 @@ server.deserializeClient(function(id, callback){
     return callback(null, client);
   });
 });
+
+// Register authorization code grant type
+server.grant(oauth2orize.grant.code(function(client, redirectUri, user, ares, callback){
+  // Create new authorization code
+  var code = new Code({
+    value: uid(16),
+    clientId: client._id,
+    redirectUri: redirectUri,
+    userId: user._id
+  });
+
+  // Save the auth code and check for errors
+  code.save(function(err){
+    if(err)
+      return callback(err);
+
+    callback(null, code.value);
+  });
+}));
+
+// Exchange authorization codes for access token
+server.exchange(oauth2orize.exchange.code(function(client, code, redirectUri, callback){
+  Code.findOne({value: code}, function(err, authCode){
+    if (err)
+      return callback(err);
+
+    if (authCode == undefined)
+      return callback(null, false);
+
+    if (client._id.toString() !== authCode.clientId)
+      return callback(null, false);
+
+    if(redirectUri !== authCode.redirectUri)
+      return callback(null, false);
+
+    // Delete auth code that it has been used
+    authCode.remove(function(err){
+      if(err)
+        return callback(url);
+
+      var token = new Token({
+        value: uid(256),
+        clientId: authCode.clientId,
+        userId: authCode.userId
+      });
+
+      token.save(function(err){
+        if(err)
+          return callback(err);
+        callback(null, token);
+      });
+    });
+  });
+}));
